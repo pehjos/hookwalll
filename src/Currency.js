eb68f8d08f95f4f162980c3244db746892fd6909
pehjos <71464455+pehjos@users.noreply.github.com>
2022-01-25 21:33:09 +0000

    update22

---
+++ src/Currency.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Currency() {
+  <script type="text/javascript" src="https://w.fxexchangerate.com/converter.php?fm=USD&ft=IQD&lg=en&am=1&ty=1">
+</script>
+  return <div>
+
+    <div style={{width:'248px',height:'auto',border:'1px solid #FFA65E',textAlign:'center',fontSize:'16px',fontFamily:'sans-serif,Arial,Helvetica',borderTopRightRadius:'5px',borderTopLeftRadius:'5px',backgroundColor:'#FFFFFF'}}>
+      <div style={{width:'100%',height:'2.2rem',padding:'5px 0px 0px 0px',backgroundColor:'#FFB33A',fontWeight:'bold'}}>
+        <a rel="nofollow" style={{color:'#FFFFFF',textDecoration:'none'}} href="">Currency Converter</a></div>
+     
+      </div>
+  </div>;
+}
+
+export default Currency;
